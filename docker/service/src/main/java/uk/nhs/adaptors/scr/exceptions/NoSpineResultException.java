ae9e4e74c2ae0131be9075d675c922e4277cf162
Bartek Sarul <b.sarul@kainos.com>
2020-11-04 17:35:17 +0100

    NIAD-712: Return 504 with proper OperationOutcome if there is no result after polling

--- docker/service/src/main/java/uk/nhs/adaptors/scr/exceptions/NoSpineResultException.java
+++ docker/service/src/main/java/uk/nhs/adaptors/scr/exceptions/NoSpineResultException.java
@@ -1,6 +1,9 @@
 package uk.nhs.adaptors.scr.exceptions;
 
 import lombok.Getter;
+import org.hl7.fhir.instance.model.api.IBaseOperationOutcome;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.OperationOutcome;
 
 @Getter
 public class NoSpineResultException extends GatewayTimeoutException {
@@ -10,5 +13,18 @@ public class NoSpineResultException extends GatewayTimeoutException {
     public NoSpineResultException(long retryAfter) {
         super("Spine polling yield no result");
         this.retryAfter = retryAfter;
+    }
+
+    @Override
+    public IBaseOperationOutcome getOperationOutcome() {
+        var operationOutcome = new OperationOutcome();
+
+        operationOutcome.addIssue()
+            .setSeverity(OperationOutcome.IssueSeverity.ERROR)
+            .setCode(OperationOutcome.IssueType.TRANSIENT)
+            .setDetails(new CodeableConcept().addCoding(NHSCodings.NO_RECORD_FOUND.asCoding()))
+            .setDiagnostics("Upstream server timed out. " + getMessage());
+
+        return operationOutcome;
     }
 }
--- docker/service/src/main/java/uk/nhs/adaptors/scr/exceptions/ScrTimeoutException.java
+++ docker/service/src/main/java/uk/nhs/adaptors/scr/exceptions/ScrTimeoutException.java
@@ -1,5 +1,9 @@
 package uk.nhs.adaptors.scr.exceptions;
 
+import org.hl7.fhir.instance.model.api.IBaseOperationOutcome;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.OperationOutcome;
+
 public class ScrTimeoutException extends GatewayTimeoutException {
     private static final String MESSAGE = "Spine POST + polling GET has timed out";
 
@@ -9,5 +13,18 @@ public class ScrTimeoutException extends GatewayTimeoutException {
 
     public ScrTimeoutException(Exception e) {
         super(MESSAGE, e);
+    }
+
+    @Override
+    public IBaseOperationOutcome getOperationOutcome() {
+        var operationOutcome = new OperationOutcome();
+
+        operationOutcome.addIssue()
+            .setSeverity(OperationOutcome.IssueSeverity.ERROR)
+            .setCode(OperationOutcome.IssueType.TRANSIENT)
+            .setDetails(new CodeableConcept().addCoding(NHSCodings.NO_RECORD_FOUND.asCoding()))
+            .setDiagnostics("Request processing timed out. " + getMessage());
+
+        return operationOutcome;
     }
 }
