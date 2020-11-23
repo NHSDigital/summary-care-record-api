fa092b382e6cea91e3a2a9e477d7b6369ee7be35
elliotb <elliotb@kainos.com>
2020-11-23 13:54:58 +0000

    Added filter for adding X-Request-ID to requests

---
+++ docker/service/src/main/java/uk/nhs/adaptors/scr/config/LoggingIdFilter.java
@@ -0,0 +1,55 @@
+package uk.nhs.adaptors.scr.config;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Component
+public class LoggingIdFilter extends OncePerRequestFilter {
+
+    public static final String HEADER_NAME = "X-Request-ID";
+    public static final String MDC_KEY = "RequestId";
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
+        throws java.io.IOException, ServletException {
+        try{
+            var token = request.getHeader(HEADER_NAME);
+            if (StringUtils.isEmpty(token)) {
+                token = getRandomRequestId();
+            }
+            applyCorrelationId(token);
+            token = URLEncoder.encode(token, StandardCharsets.UTF_8);
+            response.addHeader(HEADER_NAME, token);
+            chain.doFilter(request, response);
+        }finally {
+            resetRequestId();
+        }
+    }
+
+    public void applyCorrelationId(String id) {
+        MDC.put(MDC_KEY, id);
+    }
+
+    public String getRandomRequestId() {
+        return UUID.randomUUID().toString().toUpperCase();
+    }
+
+    public void resetRequestId() {
+        MDC.remove(MDC_KEY);
+    }
+
+}
