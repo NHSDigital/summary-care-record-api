c2c322f7d733e8c8665fa49d0067385976779347
Dawid Stelmaszek <d.stelmaszek@kainos.com>
2020-12-10 14:41:05 +0100

    NIAD-509 SCR Sandbox - Dockerfile; Apigee; dead code removal; refactoring; error handling;

--- docker/service/src/main/java/uk/nhs/adaptors/scr/utils/OperationOutcomeUtils.java
+++ docker/service/src/main/java/uk/nhs/adaptors/scr/utils/OperationOutcomeUtils.java
@@ -2,39 +2,16 @@ package uk.nhs.adaptors.scr.utils;
 
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.OperationOutcome;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import uk.nhs.adaptors.scr.exceptions.NHSCodings;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import static org.hl7.fhir.r4.model.OperationOutcome.IssueSeverity.ERROR;
-import static org.hl7.fhir.r4.model.OperationOutcome.IssueType.EXCEPTION;
-import static org.hl7.fhir.r4.model.OperationOutcome.IssueType.NOTSUPPORTED;
+import org.hl7.fhir.r4.model.OperationOutcome.IssueSeverity;
+import org.hl7.fhir.r4.model.OperationOutcome.IssueType;
 
 public class OperationOutcomeUtils {
-    public static OperationOutcome createFromException(Exception exception) {
+    public static OperationOutcome createOperationOutcome(IssueType type, IssueSeverity severity, String message) {
         var operationOutcome = new OperationOutcome();
         operationOutcome.addIssue()
-            .setCode(EXCEPTION)
-            .setSeverity(ERROR)
-            .setDetails(new CodeableConcept().setText(formatException(exception)));
+            .setCode(type)
+            .setSeverity(severity)
+            .setDetails(new CodeableConcept().setText(message));
         return operationOutcome;
-    }
-
-    public static OperationOutcome createFromMediaTypeNotSupportedException
-        (HttpMediaTypeNotSupportedException exception) {
-        var operationOutcome = new OperationOutcome();
-        operationOutcome.addIssue()
-            .setCode(NOTSUPPORTED)
-            .setSeverity(ERROR)
-            .setDetails(new CodeableConcept().setText(formatException(exception)));
-        return operationOutcome;
-    }
-
-    private static String formatException(Throwable exception) {
-        var errors = new StringWriter();
-        exception.printStackTrace(new PrintWriter(errors));
-        return errors.toString();
     }
 }
