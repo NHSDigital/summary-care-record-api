b8cbb60c486942c51027e19d9bf98ec1c0a4f3ed
mrogowska <m.rogowska@kainos.com>
2021-10-22 09:59:32 +0200

    NIAD-1415: New attempt

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -3,6 +3,7 @@ from time import time
 from uuid import uuid4
 import asyncio
 import pytest
+from tests import conftest
 from aiohttp import ClientResponse
 from api_test_utils import env
 from api_test_utils import poll_until
@@ -11,18 +12,6 @@ from api_test_utils.api_session_client import APISessionClient
 from api_test_utils.api_test_session_config import APITestSessionConfig
 from api_test_utils.oauth_helper import OauthHelper
 from environment import EnvVarWrapper
-
-
-ENV = EnvVarWrapper(
-    **{
-        'client_id': 'CLIENT_ID',
-        'client_secret': 'CLIENT_SECRET',
-        'redirect_uri': 'REDIRECT_URI',
-        'oauth_proxy': 'OAUTH_PROXY',
-        'oauth_base_uri': 'OAUTH_BASE_URI',
-        'apigee_product': 'APIGEE_PRODUCT'
-    }
-)
 
 
 def _dict_path(raw, path: List[str]):
@@ -39,41 +28,65 @@ def _dict_path(raw, path: List[str]):
     return _dict_path(res, path[1:])
 
 
-@pytest.mark.asyncio
-async def get_authorised_headers(app: ApigeeApiDeveloperApps):
-    custom_attributes = {
-        "jwks-resource-url": "https://raw.githubusercontent.com/NHSDigital/identity-service-jwks/main/jwks/"
-                             + "internal-dev/9baed6f4-1361-4a8e-8531-1f8426e3aba8.json",
-        "nhs-login-allowed-proofing-level": "P9"
-    }
-    api_products = [ENV['apigee_product']]
+# @pytest.mark.asyncio
+# async def get_authorised_headers(app: ApigeeApiDeveloperApps):
+#     custom_attributes = {
+#         "jwks-resource-url": "https://raw.githubusercontent.com/NHSDigital/identity-service-jwks/main/jwks/"
+#                              + "internal-dev/9baed6f4-1361-4a8e-8531-1f8426e3aba8.json",
+#         "nhs-login-allowed-proofing-level": "P9"
+#     }
+#     api_products = [ENV['apigee_product']]
+#
+#     loop = asyncio.new_event_loop()
+#     loop.run_until_complete(
+#         app.setup_app(
+#             api_products=api_products,
+#             custom_attributes=custom_attributes,
+#         )
+#     )
+#     oauth = OauthHelper(client_id=app.client_id, client_secret=app.client_secret, redirect_uri=app.callback_url)
+#     token_url = f"{ENV['oauth_base_uri']}/{ENV['oauth_proxy']}/token"
+#
+#     jwt = oauth.create_jwt(
+#         **{
+#             "kid": "test-1",
+#             "claims": {
+#                 "sub": app.client_id,
+#                 "iss": app.client_id,
+#                 "jti": str(uuid4()),
+#                 "aud": token_url,
+#                 "exp": int(time()) + 60,
+#             },
+#         }
+#     )
+#
+#     response = await oauth.get_token_response(grant_type="client_credentials", _jwt=jwt)
+#     token = response["body"]
+#     return {"Authorization": f'Bearer {token["access_token"]}'}
 
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(
-        app.setup_app(
-            api_products=api_products,
-            custom_attributes=custom_attributes,
-        )
-    )
-    oauth = OauthHelper(client_id=app.client_id, client_secret=app.client_secret, redirect_uri=app.callback_url)
-    token_url = f"{ENV['oauth_base_uri']}/{ENV['oauth_proxy']}/token"
+def _base_valid_uri(nhs_number) -> str:
+    return f"DocumentReference?patient=https://fhir.nhs.uk/Id/nhs-number|{nhs_number}&_sort=date&type="
+           + "http://snomed.info/sct|196981000000101&_count=1"
 
-    jwt = oauth.create_jwt(
-        **{
-            "kid": "test-1",
-            "claims": {
-                "sub": app.client_id,
-                "iss": app.client_id,
-                "jti": str(uuid4()),
-                "aud": token_url,
-                "exp": int(time()) + 60,
-            },
-        }
-    )
 
-    response = await oauth.get_token_response(grant_type="client_credentials", _jwt=jwt)
-    token = response["body"]
-    return {"Authorization": f'Bearer {token["access_token"]}'}
+# @pytest.mark.smoketest
+# @pytest.mark.asyncio
+# async def test_client_credentials_happy_path(test_app, api_client: APISessionClient):
+#     authorised_headers = await conftest.get_authorised_headers(test_app)
+#
+#     correlation_id = str(uuid4())
+#     authorised_headers["X-Correlation-ID"] = correlation_id
+#
+#     async with api_client.get(
+#         _base_valid_uri("9999999990"),
+#         headers=authorised_headers,
+#         allow_retries=True
+#     ) as resp:
+#         assert resp.status == 200
+#         body = await resp.json()
+#         assert "x-correlation-id" in resp.headers, resp.headers
+#         assert resp.headers["x-correlation-id"] == correlation_id
+#         assert_body(body)
 
 
 @pytest.mark.smoketest
@@ -83,25 +96,42 @@ def test_output_test_config(api_test_config: APITestSessionConfig):
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio
-async def test_wait_for_get_scr_id(api_client: APISessionClient):
-    app = ApigeeApiDeveloperApps()
-    headers = await get_authorised_headers(app)
+async def test_token_exchange_happy_path(test_app_and_product, api_client: APISessionClient):
+    test_product, test_app = test_app_and_product
 
-    async def scr_id_returned(resp: ClientResponse):
-        if resp.status != 200:
-            return False
+    token = await conftest.get_token(test_app)
+    headers = {"Authorization": f'Bearer {token["access_token"]}'}
 
+    async with api_client.get(
+                _base_valid_uri("9999999990"),
+                headers=headers,
+                allow_retries=True
+    ) as resp:
+        assert resp.status == 200, 'get scr id failed'
         body = await resp.json()
-        return body.get("resourceType") == "Bundle"
 
-    await poll_until(
-        make_request=lambda: api_client.get(
-            "DocumentReference?patient=https://fhir.nhs.uk/Id/nhs-number"
-            "|9995000180&_sort=date&type=http://snomed.info/sct|196981000000101&_count=1",
-            headers=headers),
-        until=scr_id_returned, timeout=30
-    )
-    app.destroy_app()
+
+# @pytest.mark.smoketest
+# @pytest.mark.asyncio
+# async def test_wait_for_get_scr_id(api_client: APISessionClient):
+#     app = ApigeeApiDeveloperApps()
+#     headers = await get_authorised_headers(app)
+#
+#     async def scr_id_returned(resp: ClientResponse):
+#         if resp.status != 200:
+#             return False
+#
+#         body = await resp.json()
+#         return body.get("resourceType") == "Bundle"
+#
+#     await poll_until(
+#         make_request=lambda: api_client.get(
+#             "DocumentReference?patient=https://fhir.nhs.uk/Id/nhs-number"
+#             "|9995000180&_sort=date&type=http://snomed.info/sct|196981000000101&_count=1",
+#             headers=headers),
+#         until=scr_id_returned, timeout=30
+#     )
+#     app.destroy_app()
 
 
 @pytest.mark.smoketest
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@
 import asyncio
 from time import time
 from uuid import uuid4
+from typing import List
 import os
+
 import pytest
 from api_test_utils.api_test_session_config import APITestSessionConfig
 from api_test_utils.fixtures import api_client  # pylint: disable=unused-import
@@ -22,21 +24,123 @@ def get_env(variable_name: str) -> str:
         raise RuntimeError(f"Variable is not set, Check {variable_name}.")
 
 
-@pytest.fixture(scope="session")
-def api_test_config() -> APITestSessionConfig:
-    return APITestSessionConfig()
+# @pytest.yield_fixture(scope='function')
+# @pytest.mark.asyncio
+# async def test_product():
+#     """ Setup and Teardown, create an product at the start and then destroy it at the end """
+#
+#     # create apigee instance & attach instance to class
+#     api = ApigeeApiProducts()
+#
+#     print("Creating Test Product..")
+#     await api.create_new_product()
+#
+#     yield api
+#     # teardown
+#     print("Destroying Test Product..")
+#     await api.destroy_product()
+#
+#
+# @pytest.yield_fixture(scope='function')
+# @pytest.mark.asyncio
+# async def test_app():
+#     """ Setup and Teardown, create an app at the start and then destroy it at the end """
+#
+#     # create apigee instance & attach instance to class
+#     api = ApigeeApiDeveloperApps()
+#
+#     print("Creating Test App..")
+#     await api.create_new_app() # callback_url=get_env("REDIRECT_URI") czy to potrzebne?
+#
+#     yield api
+#     # teardown
+#     print("Destroying Test App..")
+#     await api.destroy_app()
 
 
-# @pytest.fixture(scope="session")
-# def test_app():
-#     """Setup & Teardown an app-restricted app for this api"""
-#     app = ApigeeApiDeveloperApps()
-#     loop = asyncio.new_event_loop()
-#     loop.run_until_complete(
-#         app.setup_app(
-#             api_products=[get_env("APIGEE_PRODUCT")],
-#         )
-#     )
-#     yield app
-#     loop.run_until_complete(app.destroy_app())
+@pytest.fixture()
+@pytest.mark.asyncio
+async def test_app_and_product(self):
+    apigee_product = ApigeeApiProducts()
+    await apigee_product.create_new_product()
+    await test_product.update_scopes(['urn:nshd:apim:app:jwks', "test_scope:USER",
+                                      f"urn:nhsd:apim:app:level3:{get_env('APIGEE_PRODUCT')}"])
 
+    apigee_app = ApigeeApiDeveloperApps()
+    apigee_app.setup_app(
+        api_products=[apigee_product.name],
+        custom_attributes={
+            "jwks-resource-url": "https://raw.githubusercontent.com/NHSDigital/identity-service-jwks/main/jwks"
+            + "/internal-dev/9baed6f4-1361-4a8e-8531-1f8426e3aba8.json"
+        },
+    )
+#     await apigee_app.create_new_app() # callback_url=get_env("REDIRECT_URI") czy to potrzebne?
+
+    apigee_app.oauth = OauthHelper(apigee_app.client_id, apigee_app.client_secret, apigee_app.callback_url)
+    yield apigee_product, apigee_app
+
+    await apigee_app.destroy_app()
+    await apigee_product.destroy_product()
+
+
+def nhs_login_subject_token(test_app: ApigeeApiDeveloperApps) -> str:
+    id_token_claims = {
+        "aud": "tf_-APIM-1",
+        "id_status": "verified",
+        "token_use": "id",
+        "auth_time": 1616600683,
+        "iss": "https://internal-dev.api.service.nhs.uk",  # Points to internal dev -> testing JWKS
+        "sub": "https://internal-dev.api.service.nhs.uk",
+        "exp": int(time()) + 300,
+        "iat": int(time()) - 10,
+        "vtm": "https://auth.sandpit.signin.nhs.uk/trustmark/auth.sandpit.signin.nhs.uk",
+        "jti": str(uuid4()),
+        "identity_proofing_level": "P9",
+        "birthdate": "1939-09-26",
+        "nhs_number": "9912003888",
+        "nonce": "randomnonce",
+        "surname": "CARTHY",
+        "vot": "P9.Cp.Cd",
+        "family_name": "CARTHY",
+    }
+
+    id_token_headers = {
+        "kid": "nhs-login",
+        "typ": "JWT",
+        "alg": "RS512",
+    }
+
+    # private key we retrieved from earlier
+    nhs_login_id_token_private_key_path = get_env("ID_TOKEN_NHS_LOGIN_PRIVATE_KEY_ABSOLUTE_PATH")
+
+    with open(nhs_login_id_token_private_key_path, "r") as f:
+        contents = f.read()
+
+    id_token_jwt = test_app.oauth.create_id_token_jwt(
+        algorithm="RS512",
+        claims=id_token_claims,
+        headers=id_token_headers,
+        signing_key=contents,
+    )
+
+    return subject_token
+
+
+@pytest.mark.asyncio
+async def get_token(test_app: ApigeeApiDeveloperApps):
+    subject_token = nhs_login_subject_token(test_app)
+    client_assertion_jwt = test_app.oauth.create_jwt(kid="test-1")
+
+    token_resp = await test_app.oauth.get_token_response(
+        grant_type="token_exchange",
+        data={
+            "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
+            "subject_token_type": "urn:ietf:params:oauth:token-type:id_token",
+            "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+            "subject_token": subject_token,
+            "client_assertion": client_assertion_jwt,
+        },
+    )
+    assert token_resp["status_code"] == 200
+
+    return token_resp["body"]
