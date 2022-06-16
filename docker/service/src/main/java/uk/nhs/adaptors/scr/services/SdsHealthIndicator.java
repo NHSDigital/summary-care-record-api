7a41f3820934daaacbd1d77137295040c7a6c814
= <contact@karsten-lloyd.co.uk>
2022-06-16 14:28:30 +0100

    Added custom healthcheck to call the sds healthcheck endpoint

---
+++ docker/service/src/main/java/uk/nhs/adaptors/scr/services/SdsHealthIndicator.java
@@ -0,0 +1,29 @@
+package uk.nhs.adaptors.scr.services;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class SdsHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "http://localhost:9001/healthcheck";
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                return Health.up().build();
+            } else {
+                return Health.down().withDetail("Error", "Service is down").build();
+            }
+        } catch (Exception e) {
+            return Health.down().withDetail("Error", "Service Unavailable").build();
+        }
+    }
+}
