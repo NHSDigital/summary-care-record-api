36e8c9946aa5f242ea07adcb5ea8255366b9e079
ellenwootten <ellen.wootten@madetech.com>
2022-07-14 15:14:25 +0100

    Update SdsServiceTest.java

--- docker/service/src/test/java/uk/nhs/adaptors/scr/services/SdsServiceTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/services/SdsServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.nhs.adaptors.scr.clients.sds.SdsClient;
 import uk.nhs.adaptors.scr.components.FhirParser;
 import uk.nhs.adaptors.scr.config.SdsConfiguration;
 
