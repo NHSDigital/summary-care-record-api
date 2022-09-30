83511d3d66350e15064c96bea86dcd4a30b7577a
ellenwootten <ellen.wootten@madetech.com>
2022-09-30 10:40:37 +0100

    care event mapper from fhir
    (final ut not passing)

--- docker/service/src/main/java/uk/nhs/adaptors/scr/mappings/from/fhir/CareEventMapper.java
+++ docker/service/src/main/java/uk/nhs/adaptors/scr/mappings/from/fhir/CareEventMapper.java
@@ -2,11 +2,12 @@ package uk.nhs.adaptors.scr.mappings.from.fhir;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Encounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.nhs.adaptors.scr.models.xml.CareEvent;
+
+import static uk.nhs.adaptors.scr.utils.DateUtil.formatDateToHl7;
 
 @Slf4j
 @Component
@@ -16,6 +17,16 @@ public class CareEventMapper {
         var careEvent = new CareEvent();
 
         careEvent.setIdRoot("0F582D91-8F89-11EA-8B2D-B741F13EFC47");
+        careEvent.setCodeCode(encounter.getType().get(0).getCoding().get(0).getCode());
+        careEvent.setCodeDisplayName(encounter.getType().get(0).getCoding().get(0).getDisplay());
+        careEvent.setStatusCodeCode("normal");
+
+        if(encounter.getPeriod().hasStart()) {
+            careEvent.setEffectiveTimeHigh(formatDateToHl7(encounter.getPeriod().getStartElement()));
+        }
+        if(encounter.getPeriod().hasEnd()) {
+            careEvent.setEffectiveTimeLow(formatDateToHl7(encounter.getPeriod().getEndElement()));
+        }
 
         return careEvent;
     }
--- docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/CareEventMapperTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/CareEventMapperTest.java
@@ -1,5 +1,6 @@
 package uk.nhs.adaptors.scr.mappings.from.fhir;
 
+import com.github.mustachejava.Mustache;
 import lombok.SneakyThrows;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Encounter;
@@ -16,6 +17,7 @@ import uk.nhs.adaptors.scr.components.FhirParser;
 import uk.nhs.adaptors.scr.mappings.from.fhir.CareEventMapper;
 import uk.nhs.adaptors.scr.mappings.from.hl7.common.CodedEntryMapper;
 import uk.nhs.adaptors.scr.mappings.from.hl7.common.UuidWrapper;
+import uk.nhs.adaptors.scr.utils.TemplateUtils;
 import uk.nhs.adaptors.scr.utils.XmlUtils;
 import uk.nhs.utils.CareEventMapperArgumentsProvider;
 
@@ -37,15 +39,72 @@ public class CareEventMapperTest {
 
     @ParameterizedTest(name = "[{index}] - {0}.html/json")
     @ArgumentsSource(CareEventMapperArgumentsProvider.class)
-    public void When_MappingFromFHIR_Expect_CareEvent(String fileName) {
-        var html = parseXml(readResourceFile(String.format("care_event/%s.html", fileName))).getDocumentElement();
-        var expectedJson = readResourceFile(String.format("care_event/%s.json", fileName));
+    public void When_MappingFromFHIR_Expect_RootId(String fileName) {
+        var expectedHtml = parseXml(readResourceFile(String.format("care_event/%s.html", fileName))).getDocumentElement();
+        var json = readResourceFile(String.format("care_event/%s.json", fileName));
 
-        var actualJson = fhirParser.parseResource(expectedJson, Encounter.class);
+        var encounter = fhirParser.parseResource(json, Encounter.class);
 
-        var result = careEvent.mapCareEvent(actualJson);
+        var result = careEvent.mapCareEvent(encounter);
 
         assertThat(result.getIdRoot()).isEqualTo("0F582D91-8F89-11EA-8B2D-B741F13EFC47");
+    }
+
+    @ParameterizedTest(name = "[{index}] - {0}.html/json")
+    @ArgumentsSource(CareEventMapperArgumentsProvider.class)
+    public void When_MappingFromFHIR_Expect_Code(String fileName) {
+        var expectedHtml = parseXml(readResourceFile(String.format("care_event/%s.html", fileName))).getDocumentElement();
+        var json = readResourceFile(String.format("care_event/%s.json", fileName));
+
+        var encounter = fhirParser.parseResource(json, Encounter.class);
+
+        var result = careEvent.mapCareEvent(encounter);
+
+        assertThat(result.getCodeCode()).isEqualTo("1240631000000102");
+        assertThat(result.getCodeDisplayName()).isEqualTo("Did not attend SARS-CoV-2 (severe acute respiratory syndrome coronavirus 2) vaccination");
+    }
+
+    @ParameterizedTest(name = "[{index}] - {0}.html/json")
+    @ArgumentsSource(CareEventMapperArgumentsProvider.class)
+    public void When_MappingFromFHIR_Expect_StatusCode(String fileName) {
+        var expectedHtml = parseXml(readResourceFile(String.format("care_event/%s.html", fileName))).getDocumentElement();
+        var json = readResourceFile(String.format("care_event/%s.json", fileName));
+
+        var encounter = fhirParser.parseResource(json, Encounter.class);
+
+        var result = careEvent.mapCareEvent(encounter);
+
+        assertThat(result.getStatusCodeCode()).isEqualTo("normal");
+    }
+
+    @ParameterizedTest(name = "[{index}] - {0}.html/json")
+    @ArgumentsSource(CareEventMapperArgumentsProvider.class)
+    public void When_MappingFromFHIR_Expect_EffectiveTimeLow(String fileName) {
+        var expectedHtml = parseXml(readResourceFile(String.format("care_event/%s.html", fileName))).getDocumentElement();
+        var json = readResourceFile(String.format("care_event/%s.json", fileName));
+
+        var encounter = fhirParser.parseResource(json, Encounter.class);
+
+        var result = careEvent.mapCareEvent(encounter);
+
+        assertThat(result.getEffectiveTimeLow()).isEqualTo("20200805");
+    }
+
+    @ParameterizedTest(name = "[{index}] - {0}.html/json")
+    @ArgumentsSource(CareEventMapperArgumentsProvider.class)
+    public void When_MappingFromFHIR_Expect_MatchingHtml(String fileName) {
+        var expectedHtml = readResourceFile(String.format("care_event/%s.html", fileName));
+        var json = readResourceFile(String.format("care_event/%s.json", fileName));
+
+        var encounter = fhirParser.parseResource(json, Encounter.class);
+
+        var result = careEvent.mapCareEvent(encounter);
+
+
+        var REPC_RM150007UK05_TEMPLATE = TemplateUtils.loadTemplate("UKCT_MT144037UK01.CareEvent");
+
+        var resultStr = TemplateUtils.fillTemplate(REPC_RM150007UK05_TEMPLATE, result);
+        assertThat(resultStr).isEqualTo(expectedHtml);
     }
 
     @SneakyThrows
