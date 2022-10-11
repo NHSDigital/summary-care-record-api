b93a81db6d504962e7a04a97b7da56d10eab8f6f
abidmajid1 <abid.majid@madetech.com>
2022-10-11 09:39:23 +0100

    added mustache files for InvestigationTest

--- docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/InvestigationTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/InvestigationTest.java
@@ -9,7 +9,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.nhs.adaptors.scr.components.FhirParser;
 import uk.nhs.adaptors.scr.mappings.from.common.UuidWrapper;
+import uk.nhs.adaptors.scr.models.GpSummary;
+import uk.nhs.adaptors.scr.models.xml.CareEvent;
+import uk.nhs.adaptors.scr.models.xml.Investigation;
+import uk.nhs.adaptors.scr.utils.TemplateUtils;
 import uk.nhs.utils.InvestigationMapperArgumentsProvider;
+
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -77,27 +83,27 @@ public class InvestigationTest {
 
         assertThat(result.getEffectiveTimeLow()).isEqualTo("20200805");
     }
-//
-//    @ParameterizedTest(name = "[{index}] - {0}.html/json")
-//    @ArgumentsSource(CareEventMapperArgumentsProvider.class)
-//    public void When_MappingFromFHIR_Expect_MatchingHtml(String fileName) {
-//        var expectedHtml = readResourceFile(String.format("investigation/%s.html", fileName));
-//        var json = readResourceFile(String.format("investigation/%s.json", fileName));
-//
-//        when(uuid.randomUuid()).thenReturn("0F582D91-8F89-11EA-8B2D-B741F13EFC47");
-//
-//        var encounter = fhirParser.parseResource(json, Encounter.class);
-//
-//        var result = careEvent.mapCareEvent(encounter);
-//
-//        var gpSummary = new GpSummary();
-//        var careEvents = new ArrayList<CareEvent>();
-//        careEvents.add(result);
-//        gpSummary.setCareEvents(careEvents);
-//
-//        var careEventsTemplate = TemplateUtils.loadPartialTemplate("CareEvents.mustache");
-//
-//        var resultStr = TemplateUtils.fillTemplate(careEventsTemplate, gpSummary);
-//        assertThat(resultStr).isEqualTo(expectedHtml);
-//    }
+
+    @ParameterizedTest(name = "[{index}] - {0}.html/json")
+    @ArgumentsSource(InvestigationMapperArgumentsProvider.class)
+    public void When_MappingFromFHIR_Expect_MatchingHtml(String fileName) {
+        var expectedHtml = readResourceFile(String.format("investigation/%s.html", fileName));
+        var json = readResourceFile(String.format("investigation/%s.json", fileName));
+
+        when(uuid.randomUuid()).thenReturn("0F5A9E75-8F89-11EA-8B2D-B741F13EFC47");
+
+        var procedure = fhirParser.parseResource(json, Procedure.class);
+
+        var result = investigation.mapInvestigation(procedure);
+
+        var gpSummary = new GpSummary();
+        var investigations = new ArrayList<Investigation>();
+        investigations.add(result);
+        gpSummary.setInvestigations(investigations);
+
+        var investigationsTemplate = TemplateUtils.loadPartialTemplate("Investigations.mustache");
+
+        var resultStr = TemplateUtils.fillTemplate(investigationsTemplate, gpSummary);
+        assertThat(resultStr).isEqualTo(expectedHtml);
+    }
 }
