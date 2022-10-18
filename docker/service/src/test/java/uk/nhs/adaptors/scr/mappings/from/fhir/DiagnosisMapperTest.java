0acd99679f88a4eb61713ed3ef99404bc0d44c09
abidmajid1 <abid.majid@madetech.com>
2022-10-18 16:40:07 +0100

    JSON example refactor and test class initiated

--- docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/DiagnosisMapperTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/DiagnosisMapperTest.java
@@ -1,4 +1,45 @@
 package uk.nhs.adaptors.scr.mappings.from.fhir;
 
+import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Encounter;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.nhs.adaptors.scr.components.FhirParser;
+import uk.nhs.adaptors.scr.mappings.from.common.UuidWrapper;
+import uk.nhs.utils.DiagnosisMapperArgumentsProvider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static uk.nhs.utils.Utils.readResourceFile;
+
+@ExtendWith(MockitoExtension.class)
 public class DiagnosisMapperTest {
+
+    @InjectMocks
+    private DiagnosisMapper diagnosisMapper;
+
+    @Mock
+    private UuidWrapper uuid;
+
+    private FhirParser fhirParser = new FhirParser();
+
+    @ParameterizedTest(name = "[{index}] - {0}.html/json")
+    @ArgumentsSource(DiagnosisMapperArgumentsProvider.class)
+    public void When_MappingFromFHIR_Expect_RootId(String fileName) {
+        var json = readResourceFile(String.format("diagnosis/%s.json", fileName));
+
+        when(uuid.randomUuid()).thenReturn("AF0AAF00-797C-11EA-B378-F1A7EC384595");
+
+        var condition = fhirParser.parseResource(json, Condition.class);
+
+        var result = diagnosisMapper.mapDiagnosis(condition);
+
+        assertThat(result.getIdRoot()).isEqualTo("AF0AAF00-797C-11EA-B378-F1A7EC384595");
+    }
+
+
 }
---
+++ docker/service/src/test/java/uk/nhs/utils/DiagnosisMapperArgumentsProvider.java
@@ -0,0 +1,30 @@
+package uk.nhs.utils;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class DiagnosisMapperArgumentsProvider implements ArgumentsProvider {
+
+    @SneakyThrows
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+        return Arrays.stream(getResources())
+            .map(Resource::getFilename)
+            .map(fileName -> fileName.substring(0, fileName.lastIndexOf('.')))
+            .map(Arguments::of);
+    }
+
+    protected Resource[] getResources() throws IOException {
+        ClassLoader cl = this.getClass().getClassLoader();
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(cl);
+        return resolver.getResources("classpath*:/diagnosis/*.html");
+    }
+}
