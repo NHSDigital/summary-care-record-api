11c6dc113e8a1c94e03ae5f542d3af61848f4eb0
abidmajid1 <abid.majid@madetech.com>
2022-10-31 16:54:15 +0000

    WIP

--- docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/PatientAndCarersCorrespondenceMapperTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/PatientAndCarersCorrespondenceMapperTest.java
@@ -41,4 +41,20 @@ public class PatientAndCarersCorrespondenceMapperTest {
 
     }
 
+    @ParameterizedTest(name = "[{index}] - {0}.html/json")
+    @ArgumentsSource(PatientCarerCorrMapperArgumentsProvider.class)
+    public void When_MappingFromFHIR_Expect_Code(String fileName) {
+        var json = readResourceFile(String.format("patient_carer_correspondence/%s.json", fileName));
+
+        when(uuid.randomUuid()).thenReturn("0F582D83-8F89-11EA-8B2D-B741F13EFC47");
+
+        var communication = fhirParser.parseResource(json, Communication.class);
+
+        var result = patientCarerCorrMapper.mapPatientCarerCorrespondence(communication);
+
+        assertThat(result.getCodeCode()).isEqualTo("1240491000000103");
+        assertThat(result.getCodeDisplayName())
+            .isEqualTo("Severe acute respiratory syndrome coronavirus 2 vaccination (procedure)");
+    }
+
 }
