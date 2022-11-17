4e7d7bd2ca2d78ad8a8dcbfde1cefd0641f17c59
abidmajid1 <abid.majid@madetech.com>
2022-11-17 17:24:33 +0000

    refactor of findingMapper, created tests for InvestigationResults mappings

--- docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/ObservationMapperTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/ObservationMapperTest.java
@@ -17,12 +17,15 @@ public class ObservationMapperTest extends BaseFhirMapperTest {
     private ObservationMapper finding;
 
     private static final String RESOURCE_DIRECTORY = "finding";
-    private static final String INVESTIGATION_FILE_NAME = "investigationResults";
-    private static final String CLINICAL_OBS_FILE_NAME = "example";
+    private static final String INVESTIGATION_RESOURCE_DIRECTORY = "finding/investigation_results_expected";
+    private static final String RESOURCE_DIRECTORY_EXPECTED = "finding/expected";
+    private static final String OBSERVATION_BUNDLE_FILE = "observationsBundle";
+    private static final String INVESTIGATION_RESULTS_FILE = "investigationResults";
+    private static final String CLINICAL_OBS_FILE_NAME = "expected";
 
     @Test
     public void When_MappingFromInvestigationResultsFHIR_Expect_RootId() {
-        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, INVESTIGATION_FILE_NAME, Bundle.class);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
 
         var gpSummary = new GpSummary();
 
@@ -35,7 +38,7 @@ public class ObservationMapperTest extends BaseFhirMapperTest {
 
     @Test
     public void When_MappingFromInvestigationResultsFHIR_Expect_Code() {
-        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, INVESTIGATION_FILE_NAME, Bundle.class);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
 
         var gpSummary = new GpSummary();
 
@@ -50,7 +53,7 @@ public class ObservationMapperTest extends BaseFhirMapperTest {
 
     @Test
     public void When_MappingFromInvestigationResultsFHIR_Expect_StatusCode() {
-        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, INVESTIGATION_FILE_NAME, Bundle.class);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
 
         var gpSummary = new GpSummary();
 
@@ -63,7 +66,7 @@ public class ObservationMapperTest extends BaseFhirMapperTest {
 
     @Test
     public void When_MappingFromInvestigationResultsFHIR_Expect_EffectiveTimeLow() {
-        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, INVESTIGATION_FILE_NAME, Bundle.class);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
 
         var gpSummary = new GpSummary();
 
@@ -76,8 +79,8 @@ public class ObservationMapperTest extends BaseFhirMapperTest {
 
     @Test
     public void When_MappingFromInvestigationResultsFHIR_Expect_MatchingHtml() {
-        var expectedHtml = getExpectedHtml(RESOURCE_DIRECTORY, INVESTIGATION_FILE_NAME);
-        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, INVESTIGATION_FILE_NAME, Bundle.class);
+        var expectedHtml = getExpectedHtml(INVESTIGATION_RESOURCE_DIRECTORY, INVESTIGATION_RESULTS_FILE);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
 
         var gpSummary = new GpSummary();
 
@@ -92,7 +95,7 @@ public class ObservationMapperTest extends BaseFhirMapperTest {
     //ClinicalObservations
     @Test
     public void When_MappingFromClinicalObservationsFHIR_Expect_RootId() {
-        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, INVESTIGATION_FILE_NAME, Bundle.class);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
 
         var gpSummary = new GpSummary();
 
@@ -101,6 +104,62 @@ public class ObservationMapperTest extends BaseFhirMapperTest {
         var result = gpSummary.getClinicalObservationsAndFindings();
 
         assertThat(result.get(0).getIdRoot()).isEqualTo("19ABC6D1-8AF5-11EA-9FCE-AFDCAECF9DFB");
+    }
+
+    @Test
+    public void When_MappingFromClinicalObservationsFHIR_Expect_Code() {
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
+
+        var gpSummary = new GpSummary();
+
+        finding.mapObservations(gpSummary, jsonBundle);
+
+        var result = gpSummary.getClinicalObservationsAndFindings();
+
+        assertThat(result.get(0).getCodeCode()).isEqualTo("1240601000000108");
+        assertThat(result.get(0).getCodeDisplayName())
+            .isEqualTo("High priority for severe acute respiratory syndrome coronavirus 2 vaccination (finding)");
+    }
+
+    @Test
+    public void When_MappingFromClinicalObservationsFHIR_Expect_StatusCode() {
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
+
+        var gpSummary = new GpSummary();
+
+        finding.mapObservations(gpSummary, jsonBundle);
+
+        var result = gpSummary.getClinicalObservationsAndFindings();
+
+        assertThat(result.get(0).getStatusCodeCode()).isEqualTo("completed");
+    }
+
+    @Test
+    public void When_MappingFromClinicalObservationsFHIR_Expect_EffectiveTimeLow() {
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
+
+        var gpSummary = new GpSummary();
+
+        finding.mapObservations(gpSummary, jsonBundle);
+
+        var result = gpSummary.getClinicalObservationsAndFindings();
+
+        assertThat(result.get(0).getEffectiveTimeLow()).isEqualTo("20200805");
+    }
+
+    @Test
+    public void When_MappingFromClinicalObservationsFHIR_Expect_MatchingHtml() {
+        var expectedHtml = getExpectedHtml(RESOURCE_DIRECTORY_EXPECTED, CLINICAL_OBS_FILE_NAME);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, OBSERVATION_BUNDLE_FILE, Bundle.class);
+
+        var gpSummary = new GpSummary();
+
+        finding.mapObservations(gpSummary, jsonBundle);
+
+        var findingsTemplate = TemplateUtils.loadPartialTemplate("ClinicalObservationsAndFindings.mustache");
+
+        var resultStr = TemplateUtils.fillTemplate(findingsTemplate, gpSummary);
+        assertThat(resultStr).isEqualTo(expectedHtml);
     }
 
 
