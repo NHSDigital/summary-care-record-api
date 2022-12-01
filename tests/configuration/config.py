d655346a7f78bd71fb969dea0db8bfe143519b66
ellenwootten <ellen.wootten@madetech.com>
2022-12-01 18:10:42 +0000

    remove authenticate url

--- tests/configuration/config.py
+++ tests/configuration/config.py
@@ -7,7 +7,6 @@ ENV = EnvVarWrapper(
         'client_id': 'CLIENT_ID',
         'client_secret': 'CLIENT_SECRET',
         'redirect_uri': 'REDIRECT_URI',
-        'authenticate_url': 'AUTHENTICATE_URL',
         'service_base_path': 'SERVICE_BASE_PATH',
     }
 )
@@ -21,7 +20,6 @@ IDENTITY_SERVICE = "oauth2-no-smartcard" if ENVIRONMENT == "int" else "oauth2"
 AUTHORIZE_URL = f"{BASE_URL}/{IDENTITY_SERVICE}/authorize"
 TOKEN_URL = f"{BASE_URL}/{IDENTITY_SERVICE}/token"
 SIM_AUTH_URL = f"{BASE_URL}/{IDENTITY_SERVICE}/simulated_auth"
-AUTHENTICATE_URL = ENV["authenticate_url"]
 CALLBACK_URL = f"{BASE_URL}/{IDENTITY_SERVICE}/callback"
 
 
@@ -40,7 +38,6 @@ REDIRECT_URI = ENV["redirect_uri"]
 ENDPOINTS = {
     "authorize": AUTHORIZE_URL,
     "token": TOKEN_URL,
-    "authenticate": AUTHENTICATE_URL,
     "callback": CALLBACK_URL,
     "sim_auth": SIM_AUTH_URL,
 }
