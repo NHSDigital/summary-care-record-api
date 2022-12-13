b4a30f7bfa89466f87511d99d25495554573df8b
ellenwootten <ellen.wootten@madetech.com>
2022-12-13 17:10:50 +0000

    additional info test started

--- docker/service/src/test/java/uk/nhs/adaptors/scr/models/GpSummaryTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/models/GpSummaryTest.java
@@ -13,15 +13,63 @@ import static uk.nhs.utils.Utils.readResourceFile;
 @ExtendWith(MockitoExtension.class)
 public class GpSummaryTest {
     private static final String RESOURCE_DIRECTORY = "gp_summary";
+    private static final String HTML_RESOURCE_DIRECTORY = "gp_summary/from/fhir";
     private static final String STANDARD_FILE_NAME = "standard-gp-summary";
+    private static final String ADDITIONAL_INFO_FILE_NAME_1 = "additional-information-gp-summary-1";
     private static final String NHSD_ASID = "1029384756";
     private FhirParser fhirParser = new FhirParser();
 
     @Test
-    public void When_MappingGpSummaryFromBundle_Expect_HL7Match() {
+    public void When_MappingStandardGpSummaryFromBundle_Expect_PresentationTextHL7Match() {
+        var valueFile = "standard-gp-summary-presentation-text-value";
+        var expectedPresentationValue = readResourceFile(String.format(HTML_RESOURCE_DIRECTORY + "/%s.html", valueFile));
+
+        var jsonFile = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.json", STANDARD_FILE_NAME));
+        var bundle = fhirParser.parseResource(jsonFile, Bundle.class);
+
+        // act
+        var result = GpSummary.fromBundle(bundle, NHSD_ASID);
+
+        assertThat(result.getCompositionDate()).isEqualToIgnoringWhitespace("20201117010000");
+        assertThat(result.getPresentation().getPresentationText()).isEqualToIgnoringWhitespace(expectedPresentationValue);
+    }
+
+    @Test
+    public void When_MappingAdditionalInfoGpSummaryFromBundle_Expect_PresentationTextHL7Match() {
+        var valueFile = "additional-information-gp-summary-1-presentation-text-value";
+        var expectedPresentationValue = readResourceFile(String.format(HTML_RESOURCE_DIRECTORY + "/%s.html", valueFile));
+
+        var jsonFile = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.json", ADDITIONAL_INFO_FILE_NAME_1));
+        var bundle = fhirParser.parseResource(jsonFile, Bundle.class);
+
+        // act
+        var result = GpSummary.fromBundle(bundle, NHSD_ASID);
+
+        assertThat(result.getCompositionDate()).isEqualToIgnoringWhitespace("20200430171300");
+        assertThat(result.getPresentation().getPresentationText()).isEqualToIgnoringWhitespace(expectedPresentationValue);
+    }
+
+    @Test
+    public void When_MappingStandardGpSummaryFromBundle_Expect_HL7Match() {
         var expectedHtml = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.html", STANDARD_FILE_NAME));
 
         var jsonFile = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.json", STANDARD_FILE_NAME));
+        var bundle = fhirParser.parseResource(jsonFile, Bundle.class);
+
+        // act
+        var result = GpSummary.fromBundle(bundle, NHSD_ASID);
+
+        var gpSummaryTemplate = TemplateUtils.loadPartialTemplate("GpSummary.mustache");
+
+        var resultStr = TemplateUtils.fillTemplate(gpSummaryTemplate, result);
+        assertThat(resultStr).isEqualToIgnoringWhitespace(expectedHtml);
+    }
+
+    @Test
+    public void When_MappingAdditionalInfoGpSummaryFromBundle_Expect_HL7Match() {
+        var expectedHtml = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.html", ADDITIONAL_INFO_FILE_NAME_1));
+
+        var jsonFile = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.json", ADDITIONAL_INFO_FILE_NAME_1));
         var bundle = fhirParser.parseResource(jsonFile, Bundle.class);
 
         // act
