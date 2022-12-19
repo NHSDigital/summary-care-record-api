cbc352aa3e8c8645a8396504a75a65a9c648f8e8
ellenwootten <ellen.wootten@madetech.com>
2022-12-19 11:35:59 +0000

    lots of renaming to stay consistent

--- docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/hl7/GpSummaryMapperTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/hl7/GpSummaryMapperTest.java
@@ -29,22 +29,29 @@ public class GpSummaryMapperTest extends BaseHL7MapperTest {
     private GpSummaryMapper gpSummaryMapper;
     @Mock
     private AgentPersonMapper agentPersonMapper;
+    @Mock
+    private AgentPersonSdsMapper agentPersonSdsMapper;
+    @Mock
+    private OrganisationSdsMapper organisationSdsMapper;
+    @Mock
+    private AgentOrganisationMapper agentOrganisationMapper;
     @Spy
     private HtmlParser htmlParser = new HtmlParser(new XmlUtils(XPathFactory.newInstance()));
 
     private static final String HTML_RESOURCE_DIRECTORY = "gp_summary/from/hl7";
     private static final String PARTIALS_RESOURCE_DIRECTORY = "gp_summary/partials";
-    // private static final String RESOURCE_DIRECTORY = "gp_summary";
-    private static final String STANDARD_FILE_NAME = "standard-gp-summary";
-    // private static final String NHSD_ASID = "1029384756";
+     private static final String RESOURCE_DIRECTORY = "gp_summary";
+    private static final String STANDARD_FILE_NAME = "standard_gp_summary";
+    private static final String ADDITIONAL_INFO_FILE_NAME = "additional_information_gp_summary_1";
+    private static final String NHSD_ASID = "1029384756";
     private FhirParser fhirParser = new FhirParser();
 
     @Test
     public void When_MappingGpSummaryFromHL7_Expect_BundleMatch() {
         var html = getHtmlExample(HTML_RESOURCE_DIRECTORY, STANDARD_FILE_NAME);
-        // var expectedJson = getJsonExample(RESOURCE_DIRECTORY, STANDARD_FILE_NAME);
+         var expectedJson = getJsonExample(RESOURCE_DIRECTORY, STANDARD_FILE_NAME);
 
-        var authorResources = prepAuthorResources();
+        var authorResources = prepStandardAuthorResources();
 
         doReturn(authorResources).when(agentPersonMapper).map(any());
 
@@ -58,16 +65,49 @@ public class GpSummaryMapperTest extends BaseHL7MapperTest {
 
         verifyHtmlParserHits();
 
-        // assertThat(resultStr).isEqualToIgnoringWhitespace(expectedJson);
+         assertThat(resultStr).isEqualToIgnoringWhitespace(expectedJson);
         assertThat(resultStr).contains("Bundle");
-        assertThat("1").isEqualToIgnoringWhitespace("1");
     }
 
-    private ArrayList<? extends Resource> prepAuthorResources() {
+    @Test
+    public void When_MappingAdditionalInformationGpSummaryFromHL7_Expect_BundleMatch() {
+        var html = getHtmlExample(HTML_RESOURCE_DIRECTORY, ADDITIONAL_INFO_FILE_NAME);
+        var expectedJson = getJsonExample(RESOURCE_DIRECTORY, ADDITIONAL_INFO_FILE_NAME);
+
+        var authorResources = prepAdditionalInfoAuthorResources();
+
+        doReturn(authorResources).when(agentPersonSdsMapper).map(any());
+
+        // act
+        var results = gpSummaryMapper.map(html);
+
+        var resultBundle = new Bundle();
+        results.stream().map(resource -> getBundleEntryComponent(resource)).forEach(resultBundle::addEntry);
+
+        var resultStr = fhirParser.encodeToJson(resultBundle);
+
+        verifyHtmlParserHits();
+
+        assertThat(resultStr).isEqualToIgnoringWhitespace(expectedJson);
+        assertThat(resultStr).contains("Bundle");
+    }
+
+    private ArrayList<? extends Resource> prepStandardAuthorResources() {
         var resources = new ArrayList<Resource>();
 
         resources.add(getFileAsObject(PARTIALS_RESOURCE_DIRECTORY, "author_practitioner", Practitioner.class));
         resources.add(getFileAsObject(PARTIALS_RESOURCE_DIRECTORY, "author_practitioner_role", PractitionerRole.class));
+
+        return resources;
+    }
+
+    private ArrayList<? extends Resource> prepAdditionalInfoAuthorResources() {
+        var resources = new ArrayList<Resource>();
+
+        var filename = "additional_info_1";
+
+        resources.add(getFileAsObject(PARTIALS_RESOURCE_DIRECTORY, String.format("author_practitioner_%s", filename), Practitioner.class));
+        resources.add(getFileAsObject(PARTIALS_RESOURCE_DIRECTORY, String.format("author_practitioner_role_%s", filename), PractitionerRole.class));
 
         return resources;
     }
