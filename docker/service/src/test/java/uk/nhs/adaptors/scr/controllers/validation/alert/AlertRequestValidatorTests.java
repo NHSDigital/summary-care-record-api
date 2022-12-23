ffd5cb1475985c66cca5f3f5ec19e74a54604523
abidmajid1 <abid.majid@madetech.com>
2022-12-23 14:01:46 +0000

    Update AlertRequestValidatorTests.java

--- docker/service/src/test/java/uk/nhs/adaptors/scr/controllers/validation/alert/AlertRequestValidatorTests.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/controllers/validation/alert/AlertRequestValidatorTests.java
@@ -64,21 +64,6 @@ public class AlertRequestValidatorTests {
     }
 
     @Test
-    public void When_ValidatingTypeOutsideRange_Expect_False() {
-        // arrange
-        var json = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.json", "type_range"));
-        var outOfBoundsMessage = "Invalid or missing value in field 'type.code'. Supported values are: 1, 2";
-
-        when(mockContext.buildConstraintViolationWithTemplate(outOfBoundsMessage)).thenReturn(mockBuilder);
-
-        // act
-        var result = alertRequestValidator.isValid(json, mockContext);
-
-        // assert
-        assertThat(result).isFalse();
-    }
-
-    @Test
     public void When_AlertTypeCombination_Incorrect_Expect_False() {
         // arrange
         var json = readResourceFile(String.format(RESOURCE_DIRECTORY + "/%s.json", "alert_combination_check"));
