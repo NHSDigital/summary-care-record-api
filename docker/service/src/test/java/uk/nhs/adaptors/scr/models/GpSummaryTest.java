8655b67419a917661ab5e2f662e6d4c28f06a5b8
Alex-Nita <alexandru.nita@madetech.com>
2023-04-19 16:02:27 +0100

    Attempt 9

--- docker/service/src/test/java/uk/nhs/adaptors/scr/models/GpSummaryTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/models/GpSummaryTest.java
@@ -373,7 +373,8 @@ public class GpSummaryTest {
 
         var resultStr = TemplateUtils.fillTemplate(gpSummaryTemplate, result);
 
-        assertThat(resultStr).contains("");
+        assertThat(resultStr).isNotEmpty();
+        //assertThat(resultStr).contains("");
     }
 
 //    Below tests are to be used manually during development.
