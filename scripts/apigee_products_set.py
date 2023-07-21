90419008c25b31aebfac61f0028241153697c271
Alex-Nita <alexandru.nita@madetech.com>
2023-07-21 12:31:07 +0100

    Added new apigee script

--- scripts/apigee_products_set.py
+++ scripts/apigee_products_set.py
@@ -5,9 +5,51 @@ from pytest_nhsd_apim.apigee_apis import (
     ApigeeNonProdCredentials,
     ApiProductsAPI,
     DebugSessionsAPI,
-    DeveloperAppsAPI,
+    DeveloperAppsAPI
 )
+
+from pytest_nhsd_apim.identity_service import (
+    ClientCredentialsConfig,
+    ClientCredentialsAuthenticator,
+    AuthorizationCodeConfig,
+    AuthorizationCodeAuthenticator,
+    KeycloakUserConfig,
+    KeycloakUserAuthenticator,
+    TokenExchangeConfig,
+    TokenExchangeAuthenticator,
+)
+
 import pprint
+import requests
+
+
+def test_authorization_code_authenticator(_test_app_credentials, apigee_environment):
+    # 1. Set your app config
+    config = AuthorizationCodeConfig(
+        environment=apigee_environment,
+        identity_service_base_url=f"https://{apigee_environment}.api.service.nhs.uk/oauth2-mock",
+        callback_url="https://example.org/callback",
+        client_id=_test_app_credentials["rx35jQzKBQW1l5FxY4siA7Wr7hsuhAJb"],
+        client_secret=_test_app_credentials["xTLKl8OCWawFRXBV"],
+        scope="nhs-cis2",
+        login_form={"username": "656005750104"},
+    )
+
+    # 2. Pass the config to the Authenticator
+    authenticator = AuthorizationCodeAuthenticator(config=config)
+
+    # 3. Get your token
+    token_response = authenticator.get_token()
+    assert "access_token" in token_response
+    token = token_response["access_token"]
+
+    # 4. Use the token and confirm is valid
+    headers = {"Authorization": f"Bearer {token}"}
+    resp = requests.get(
+        f"https://{apigee_environment}.api.service.nhs.uk/mock-jwks/test-auth/nhs-cis2/aal3",
+        headers=headers,
+    )
+    assert resp.status_code == 200
 
 
 @pytest.fixture()
