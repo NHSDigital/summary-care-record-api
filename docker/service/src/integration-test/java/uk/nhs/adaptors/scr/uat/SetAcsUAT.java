11c6a8e6aa84f0bad480c5ab59fd383af7fd3eee
Alex-Nita <alexandru.nita@madetech.com>
2023-09-05 13:24:08 +0100

    Fixed integration tests

--- docker/service/src/integration-test/java/uk/nhs/adaptors/scr/uat/SetAcsUAT.java
+++ docker/service/src/integration-test/java/uk/nhs/adaptors/scr/uat/SetAcsUAT.java
@@ -4,6 +4,8 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,8 +18,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import uk.nhs.adaptors.scr.WireMockInitializer;
 import uk.nhs.adaptors.scr.consts.ScrHttpHeaders;
-
-
+import uk.nhs.adaptors.scr.uat.common.CustomArgumentsProvider.SetAcsBadRequest;
+import uk.nhs.adaptors.scr.uat.common.CustomArgumentsProvider.SetAcsSpineError;
+import uk.nhs.adaptors.scr.uat.common.CustomArgumentsProvider.SetAcsSuccess;
+import uk.nhs.adaptors.scr.uat.common.TestData;
 
 import java.io.IOException;
 
@@ -33,6 +37,8 @@ import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.TEXT_XML_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.nhs.adaptors.scr.consts.SpineHttpHeaders.SOAP_ACTION;
 import static uk.nhs.adaptors.scr.controllers.FhirMediaTypes.APPLICATION_FHIR_JSON;
 
@@ -53,6 +59,7 @@ public class SetAcsUAT {
     private static final String CLIENT_IP = "192.168.0.24";
     private static final String NHSD_SESSION_URID = "555254240100";
     private static final String USER_ID_QUERY_PARAM = "user-role-id";
+    private static final String NHSD_IDENTITY_UUID = "786333543";
 
     @Value("classpath:uat/responses/acs/success.xml")
     private Resource acsSuccessResponse;
@@ -72,9 +79,47 @@ public class SetAcsUAT {
     @Autowired
     private WireMockServer wireMockServer;
 
+    @ParameterizedTest(name = "[{index}] - {0}")
+    @ArgumentsSource(SetAcsSuccess.class)
+    public void testSetAcsPermissionViaSds(TestData testData) throws Exception {
+        stubSpineAcsEndpoint(acsSuccessResponse);
+        stubFailedIdentityService();
+        stubSdsService(practitionerRoleResponse);
 
+        performRequest(testData.getFhirRequest())
+            .andExpect(status().isCreated());
+    }
 
+    @ParameterizedTest(name = "[{index}] - {0}")
+    @ArgumentsSource(SetAcsSuccess.class)
+    public void testSetAcsPermissionViaUserInfo(TestData testData) throws Exception {
+        stubSpineAcsEndpoint(acsSuccessResponse);
+        stubIdentityService(userInfoResponse);
 
+        performRequest(testData.getFhirRequest())
+            .andExpect(status().isCreated());
+    }
+
+    @ParameterizedTest(name = "[{index}] - {0}")
+    @ArgumentsSource(SetAcsSpineError.class)
+    public void testSetAcsPermissionSpineError(TestData testData) throws Exception {
+        stubSpineAcsEndpoint(acsErrorResponse);
+        stubIdentityService(userInfoResponse);
+
+        performRequest(testData.getFhirRequest())
+            .andExpect(status().isBadRequest())
+            .andExpect(content().json(testData.getFhirResponse()));
+    }
+
+    @ParameterizedTest(name = "[{index}] - {0}")
+    @ArgumentsSource(SetAcsBadRequest.class)
+    public void testSetAcsPermissionBadRequest(TestData testData) throws Exception {
+        stubIdentityService(userInfoResponse);
+
+        performRequest(testData.getFhirRequest())
+            .andExpect(status().isBadRequest())
+            .andExpect(content().json(testData.getFhirResponse()));
+    }
 
     private ResultActions performRequest(String request) throws Exception {
         return mockMvc.perform(post(ACS_ENDPOINT)
@@ -82,6 +127,7 @@ public class SetAcsUAT {
             .header(ScrHttpHeaders.NHSD_ASID, NHSD_ASID)
             .header(ScrHttpHeaders.CLIENT_IP, CLIENT_IP)
             .header(ScrHttpHeaders.NHSD_SESSION_URID, NHSD_SESSION_URID)
+            .header(ScrHttpHeaders.NHSD_IDENTITY, NHSD_IDENTITY_UUID)
             .header(AUTHORIZATION, BEARER_TOKEN)
             .content(request));
 
