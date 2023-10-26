74d524ad3083ba118ac2566133d9151c5a3cda2d
Alex-Nita <alexandru.nita@madetech.com>
2023-10-26 15:19:42 +0100

    Added Author Mapper Test file.

---
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/fhir/AuthorMapperTest.java
@@ -0,0 +1,38 @@
+package uk.nhs.adaptors.scr.mappings.from.fhir;
+
+import org.hl7.fhir.r4.model.Bundle;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.nhs.adaptors.scr.models.GpSummary;
+import uk.nhs.adaptors.scr.utils.TemplateUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthorMapperTest extends BaseFhirMapperTest {
+
+    @InjectMocks
+    private AuthorMapper authorMapper;
+
+    private static final String RESOURCE_DIRECTORY = "author";
+    private static final String AUTHOR_BUNDLE_FILE = "authorBundle";
+    private static final String AUTHOR_HL7_FILE = "author";
+
+    @Test
+    public void When_MappingFromAuthorlessFHIR_Expect_MatchingHtml() {
+        var expectedHtml = getExpectedHtml(RESOURCE_DIRECTORY, AUTHOR_HL7_FILE);
+        var jsonBundle = getFileAsObject(RESOURCE_DIRECTORY, AUTHOR_BUNDLE_FILE, Bundle.class);
+
+        var gpSummary = new GpSummary();
+
+        authorMapper.mapAuthor(gpSummary, jsonBundle);
+
+        var findingsTemplate = TemplateUtils.loadPartialTemplate("GpSummary.mustache");
+
+        var resultStr = TemplateUtils.fillTemplate(findingsTemplate, gpSummary);
+        assertThat(resultStr).isEqualTo(expectedHtml);
+    }
+
+}
