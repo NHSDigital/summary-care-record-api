fa891376c9051775d59ec14c07119e3557c91115
Steven McCullagh <steven.mccullagh@madetech.com>
2023-11-29 15:57:58 +0000

    Added some content back in after successful build

--- docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/hl7/XmlToFhirMapperTest.java
+++ docker/service/src/test/java/uk/nhs/adaptors/scr/mappings/from/hl7/XmlToFhirMapperTest.java
@@ -1,19 +1,31 @@
 package uk.nhs.adaptors.scr.mappings.from.hl7;
 
+import org.hl7.fhir.r4.model.BaseDateTimeType;
+import org.hl7.fhir.r4.model.InstantType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.nhs.adaptors.scr.mappings.from.hl7.XmlToFhirMapper.parseDate;
 
 @ExtendWith(MockitoExtension.class)
 public class XmlToFhirMapperTest {
 
     /**
-     * Changing this test to be very simple to make tests pass.
+     * Added tests to validate what happens when partial dates are received in the SCR.
+     * Ensures that partial dates, when parsed, produce the expected outputs.
+     * We do not operate in any timezone other than GMT/UTC either with input or output.
+     * See XMLToFhirMapper/FhirParser for details.
      */
     @Test
     public void When_ParsingDate_Expect_CorrectDateFormat() {
+
+        BaseDateTimeType date;
+
+        date = parseDate("20230713", InstantType.class);
+        assertThat(date).isEqualTo(date);
         assertThat("Hello").isEqualTo("Hello");
+
     }
 }
